cb678693b35330b14a596462886e1c2fca1e836c
Kelvin Chu <kelvin.chu@gerbit.com>
2016-01-29 11:54:50 -0600

    Tuned animations #2

--- main/static/js/layout/controllers/navbarController.js
+++ main/static/js/layout/controllers/navbarController.js
@@ -2,12 +2,11 @@
     'use strict';
 
     angular.module('cravus.layout').controller('navbarController', navbarController);
-    navbarController.$inject = ['$route', 'authFactory'];
-    function navbarController($route, authFactory) {
+    navbarController.$inject = ['authFactory'];
+    function navbarController(authFactory) {
         var vm = this;
         vm.isOpen = false;
         vm.isCollapsed = true;
-        vm.route = $route;
         vm.logout = logout;
         function logout() {
             authFactory.logout();
